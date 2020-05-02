4d75df8ba00833779c89a7eced0d3b4cf4f774fe
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-05-02 18:41:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MainPage from "./components/MainPage/MainPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Gatheria</header>
+        <MainPage />
+        <footer className="component">Gatheria © </footer>
+        </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+// <div className="App">
+//         <header className="App-header">Gatheria</header>
+//         <div className="navigation">
+//           <div className="nav">
+//             <h4>Hello</h4>
+//           </div>
+//           <div className="nav">
+//             <h4>Hello Again</h4>
+//           </div>
+//         </div>
+//         <footer className="component">Gatheria © </footer>
+//       </div>
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MainPage.css"
+const MainPage = (props) => (
+  <div className="navigation">
+    <div className="nav">
+      <h4>Hello</h4>
+    </div>
+    <div className="nav">
+      <h4>Hello Again</h4>
+    </div>
+  </div>
+);
+
+export default MainPage;
